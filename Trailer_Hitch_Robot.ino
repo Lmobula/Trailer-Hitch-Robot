d8ad57e0ce19c99f72489f933a3c151bab29dfe8
Lmobula <lauramobula@gmail.com>
2016-08-31 13:01:40 -0400

    Update Trailer_Hitch_Robot.ino

--- Trailer_Hitch_Robot.ino
+++ Trailer_Hitch_Robot.ino
@@ -3,7 +3,8 @@
 const int actrCtrlPin1 = 2;             //Pin to control actuator movement
 const int actrCtrlPin2 = 3;             //Pin to control actuator movement
 const int hallSensorPin = A0;           //Pin to read Hall Effect Sensor
-
+const PIN_BUTTON_A = 20;
+const PIN_BUTTON_B = 21;
 const int limitTop = 100;               //Value from Hall Effect Sensor for max extension
 const int limitBot = 0;                 //Value from Hall Effect Sensor for max retraction
 
@@ -11,6 +12,8 @@ int motorDir = 8; //Dir 1
 int motorSpeed = 9; //PWM 1
 int motorDir2 = 10; //Dir 2
 int motorSpeed2 = 11; //PWM2
+int forward_movement;
+int backward_movement;
 
 USB Usb;
 XBOXRECV Xbox(&Usb);
@@ -24,6 +27,10 @@ void setup() {
   pinMode(motorDir2, OUTPUT); //Movement Part
   pinMode(motorSpeed, OUTPUT); //Movement Part
   pinMode(motorSpeed2, OUTPUT); //Movement Part
+   pinMode(PIN_BUTTON_A, INPUT);
+digitalWrite(PIN_BUTTON_A, LOW);
+ pinMode(PIN_BUTTON_B, INPUT);
+digitalWrite(PIN_BUTTON_B, LOW);
 }
 
 void loop() {
@@ -59,8 +66,17 @@ void loop() {
     else if(Xbox.getAnalogHat(LeftHatX, 0) < 1000) {   //Check if Right Stick is in down direction
       //going left???
     }
+    else if (digitalRead(PIN_BUTTON_A) == LOW) {
+    // Button is pressed
+    ButtonA_pressed();
+    }
+    else if (digitalRead(PIN_BUTTON_B) == LOW) {
+    // Button is pressed
+    ButtonB_pressed();
+    }
     else {                                              //If Right Stick is not moved
-      actuatorHold();
+      actuatorHold();                                
+      // Motor is at 0 for driving
     }
     delay(1);
   }
@@ -85,3 +101,29 @@ void actuatorHold() {
 
 
 //Stephanie Stuff for movement
+
+//Laura
+
+void ButtonA_pressed(){
+if (PIN_BUTTON_A) == LOW) {                        // Button A is pressed
+  both motors move forward at same speed at certain time();
+}
+  else {
+    both motors move backward at same speed at certain time();
+  }
+}
+
+void ButtonB_pressed(){
+if (PIN_BUTTON_B) == LOW) {                     // Buttom B is pressed
+  all motors stop ();
+}
+  else {
+    press BUTTON_B to start the system ();
+  }
+}
+
+
+
+
+
+
